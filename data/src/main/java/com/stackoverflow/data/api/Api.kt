dc05fbbccf2b53e86f98c7f8bd33c2a7c0b83b55
PetyaArt <artamonovpetya@gmail.com>
2019-03-16 11:51:24 +0300

    feat(code): added request for 100 items

--- data/src/main/java/com/stackoverflow/data/api/Api.kt
+++ data/src/main/java/com/stackoverflow/data/api/Api.kt
@@ -2,9 +2,10 @@ package com.stackoverflow.data.api
 
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Api {
 
-    @GET("2.2/questions?order=desc&sort=activity&tagged=android&site=stackoverflow")
+    @GET("2.2/questions?pagesize=100&order=desc&sort=activity&tagged=android&site=stackoverflow")
     fun getQuestions(): Observable<QuestionsResult>
 }
--- presentation/src/main/java/com/stackoverflow/questions/QuestionsActivity.kt
+++ presentation/src/main/java/com/stackoverflow/questions/QuestionsActivity.kt
@@ -6,11 +6,11 @@ import com.stackoverflow.R
 import com.stackoverflow.utils.ActivityUtils
 
 
-class MainActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_questions)
 
         var questionsFragment = supportFragmentManager.findFragmentById(R.id.container) as QuestionsFragment?
         if (questionsFragment == null) {
